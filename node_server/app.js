94a7a981f7ee746377ce74a2c1595d687338808a
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-02-05 02:17:48 +0900

    Update app.js

--- node_server/app.js
+++ node_server/app.js
@@ -33,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/contentList', contentListRouter)
+app.use('/create', createRouter)
 app.use('/read',readRouter)
 
 
