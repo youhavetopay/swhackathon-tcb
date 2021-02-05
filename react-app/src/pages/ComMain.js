06b504118f537a79b6b23ab3348bafed1d1684ef
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2021-02-06 04:57:49 +0900

    complete commain.js
    commain 완성

--- react-app/src/pages/ComMain.js
+++ react-app/src/pages/ComMain.js
@@ -1,27 +1,23 @@
 import { useDebugValue } from 'react'
 import './ComMain.css'
+import mainPersonImg from '../images/commain_person.png'
+import mainLogImg from '../images/commain_log.png'
 
 
 function ComMain() {
-
-
-
     return(
         <div className="body">
             <div className="flex_row">
                 <div style={{flexGrow:1}}>
                     <div className="flex_column left_box">
-                        <div style={{flexGrow:5}} className="profile">
-                            <img id="profile_pic" src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png' width='150'></img>
-                            <p>userId</p>
-                            <p>testValue</p>
+                        <div style={{flexGrow:5}} className="profile flex_column">
+                            <img id="main_log" src={mainLogImg} width="300"></img>
+                            <img id="profile_pic" src='https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png' width='250'></img>
+                            <span>userId</span>
+                            <span>이름</span>
+                            <button id="logout_btn">Logout</button>
                         </div>
-                        <div style={{flexGrow:1}} className="letf_box_bottom">
-                            <div>Home</div>
-                            <div>글 목록</div>
-                            <div>글 쓰기</div>
-                            <div style={{marginBottom:0}}>Setting</div>
-                        </div>
+                        
                     </div>
                 </div>
                 <div style={{flexGrow:2}}>
@@ -36,7 +32,7 @@ function ComMain() {
                         <span>시작입니다.</span>
                         </div>
                         <div className="mainBox_text" style={{flexGrow:1}}>
-                            <p>사진</p>
+                            <img src={mainPersonImg} width='300'></img>
                         </div>
                     </div>
                 </div>
