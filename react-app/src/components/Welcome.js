a59e34865470afc63b733fdd28444c42461666a8
Sigening <termsigening97@gmail.com>
2021-02-05 14:28:45 +0900

    Remvoed "Register" Page

--- react-app/src/components/Welcome.js
+++ react-app/src/components/Welcome.js
@@ -1,7 +1,6 @@
 import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom"
 import Main from "../pages/Main"
 import Login from "../pages/Login"
-import Register from "../pages/Register"
 import WritePost from "../pages/WritePost"
 import NoPageFound from "../pages/NoPageFound"
 
@@ -15,9 +14,6 @@ function Welcome(){
                 <Link to="/login">
                     <button>Login</button>
                 </Link>
-                <Link to="/register">
-                    <button>Register</button>
-                </Link>
                 <Link to="/write">
                     <button>Test</button>
                 </Link>
@@ -27,7 +23,6 @@ function Welcome(){
                 <Switch>
                     <Route exact path="/" component={Main} />
                     <Route path="/login" component={Login} />
-                    <Route path="/register" component={Register} />
                     <Route path="/write" component={WritePost} />
                     <Route component={NoPageFound} />
                 </Switch>
