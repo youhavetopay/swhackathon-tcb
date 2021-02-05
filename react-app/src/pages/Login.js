9037e7b684dcf6d3cd2513cf4eb825d3953ac588
Jungahn Yang <duckzza0921@gmail.com>
2021-02-05 15:23:23 +0900

    login page

--- react-app/src/pages/Login.js
+++ react-app/src/pages/Login.js
@@ -1,12 +1,36 @@
-function Login() {
-    return(
-        <div>
-            <p>
-                Login Page
-            </p>
-            <img src='https://developers.kakao.com/tool/resource/static/img/button/login/full/ko/kakao_login_medium_narrow.png' alt='Kakao'></img>
-        </div>
-    );
-}
+import React from "react";
+import { Link } from 'react-router-dom'
+//import loginImg from "../../login.svg";
 
-export default Login;
+
+
+export class Login extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+  
+    render() {
+      return (
+        <div className="base-container" ref={this.props.containerRef}>
+          <div className="header">SNS Login</div>
+          <div className="content">
+            {/* <div className="image">
+              <img src={loginImg} />
+            </div> */}
+            <div className="form">
+              <button type="button" className="btn"><img src='https://developers.kakao.com/tool/resource/static/img/button/login/full/ko/kakao_login_medium_narrow.png' alt='Kakao'></img></button><br></br>
+              <button type="button" className="btn">페이스북 로그인</button><br></br>
+              <button type="button" className="btn">카카오톡 로그인</button><br></br>
+            </div>
+          </div>
+          <div className="footer">
+            <Link to="./Register"> 
+              <button> 회원가입 </button>
+            </Link>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  export default Login;
